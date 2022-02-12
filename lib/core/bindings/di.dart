20a77333ba06e27d066b83cf6b4402cc2db306a4
Iandi Santulus <iandisantulusn@gmail.com>
2022-02-13 06:58:28 +0700

    chore(bindings): add all bindings

---
+++ lib/core/bindings/di.dart
@@ -0,0 +1,7 @@
+import '/features/auth/bindings/auth_di.dart';
+import '/features/home/bindings/home_di.dart';
+
+void initDI() {
+  initAuthDI();
+  initHomeDI();
+}
---
+++ lib/core/bindings/routes.dart
@@ -0,0 +1,7 @@
+import '/features/auth/bindings/auth_routes.dart';
+import '/features/home/bindings/home_routes.dart';
+
+void initRoutes() {
+  initAuthRoutes();
+  initHomeRoutes();
+}
